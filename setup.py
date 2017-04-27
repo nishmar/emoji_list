adcfad069ae18f9840a454d2eba863967fa00470
Vincent Wong <vmwong1@gmail.com>
2017-04-27 16:20:11 -0400

    Added pypi.io requirements. Renamed package to remove hyphen for clarity

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
-setup(name='emoji-list',
+setup(name='emoji_list',
       version='0.50',
       description="Raw lists of emoji by type, stripped so you don't have to",
-      url='http://github.com/vincentmwong/emoji-list',
+      long_description=(''
+                      ''),
+      url='http://github.com/vincentmwong/emoji_list',
       author='Vincent Wong',
       author_email='vmwong1@gmail.com',
       license='MIT',
-      packages=['emoji-list'],
+      packages=['emoji_list'],
+      keywords='emoji', 
       zip_safe=False)
